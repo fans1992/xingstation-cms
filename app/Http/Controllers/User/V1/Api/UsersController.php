a3d66da153cf0e06272a8fbb8b7ef0e1222eb061
fanzhuo <henbuhai@hotmail.com>
2019-06-03 11:01:48 +0800

    user info api

--- app/Http/Controllers/User/V1/Api/UsersController.php
+++ app/Http/Controllers/User/V1/Api/UsersController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers\User\V1\Api;
 
 use App\Http\Controllers\Admin\Common\V1\Models\Image;
+use App\Http\Controllers\User\V1\Transformer\CustomerTransformer;
 use App\Http\Controllers\User\V1\Transformer\UserTransformer;
 use App\Http\Controllers\User\V1\Request\UserRequest;
 use App\Http\Controllers\Controller;
-use App\Models\User;
 
 class UsersController extends Controller
 {
     public function me()
     {
         //$this->user() dingo api helper trait
-        return $this->response->item($this->user(), new UserTransformer());
+        return $this->response->item($this->user(), new CustomerTransformer());
     }
 
 
